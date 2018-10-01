f4ddf50024aad815c7b9d2e07ffbe0d075643d17
Gian Franco Grillo <gfgrillo3@gmail.com>
2018-10-01 14:01:10 -0300

    prueba Merge a master

--- src/main/java/com/ungs/formar/vista/SeleccionarSala.java
+++ src/main/java/com/ungs/formar/vista/SeleccionarSala.java
@@ -24,91 +24,7 @@ public class SeleccionarSala extends JFrame {
 	private DefaultTableModel modelTemas;
 	private  String[] nombreColumnas = {"Numero", "Nombre", "Capacidad"};
 	private JTable tablaSalas;
-<<<<<<< HEAD
 
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SeleccionarSala frame = new SeleccionarSala();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public SeleccionarSala() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 518, 432);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JScrollPane spSalas = new JScrollPane();
-		spSalas.setBounds(10, 141, 482, 206);
-		contentPane.add(spSalas);
-		
-		modelTemas = new DefaultTableModel(null,nombreColumnas);
-		tablaSalas = new JTable(modelTemas);
-		tablaSalas.setFont(new Font("Arial", Font.PLAIN, 12));
-		/*
-		tablaUsuarios.getColumnModel().getColumn(0).setPreferredWidth(100);
-		tablaUsuarios.getColumnModel().getColumn(0).setResizable(false);
-		*/
-		
-		spSalas.setViewportView(tablaSalas);
-		
-		JButton btnSeleccionar = new JButton("SELECCIONAR");
-		btnSeleccionar.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnSeleccionar.setBounds(10, 358, 199, 23);
-		contentPane.add(btnSeleccionar);
-		
-		JButton btnCancelar = new JButton("CANCELAR");
-		btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnCancelar.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnCancelar.setBounds(293, 359, 199, 23);
-		contentPane.add(btnCancelar);
-		
-		JLabel lblFiltrarPor = new JLabel("CAPACIDAD:");
-		lblFiltrarPor.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblFiltrarPor.setBounds(10, 15, 131, 14);
-		contentPane.add(lblFiltrarPor);
-		
-		JComboBox comboFiltrar = new JComboBox();
-		comboFiltrar.setFont(new Font("Arial", Font.PLAIN, 12));
-		comboFiltrar.setBounds(151, 9, 178, 20);
-		contentPane.add(comboFiltrar);
-		
-		JComboBox comboOrdenarPor = new JComboBox();
-		comboOrdenarPor.setFont(new Font("Arial", Font.PLAIN, 12));
-		comboOrdenarPor.setBounds(151, 65, 178, 20);
-		contentPane.add(comboOrdenarPor);
-		
-		JLabel lblOrdenarPor = new JLabel("ORDENAR POR:");
-		lblOrdenarPor.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblOrdenarPor.setBounds(10, 68, 131, 14);
-		contentPane.add(lblOrdenarPor);
-		
-		JLabel lblSalas = new JLabel("SALAS");
-		lblSalas.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSalas.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblSalas.setBounds(10, 106, 482, 14);
-		contentPane.add(lblSalas);
-	}
-=======
 	private JLabel lblSalas;
 
 	
@@ -189,5 +105,4 @@ public class SeleccionarSala extends JFrame {
 		contentPane.add(lblSalas);
 	}
 
->>>>>>> refs/heads/master
 }
