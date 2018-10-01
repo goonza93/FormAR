a6c003213ff940bb18e117171bf9c31b0c51d460
Gian Franco Grillo <gfgrillo3@gmail.com>
2018-10-01 15:59:54 -0300

    Curso Entidad

--- src/main/java/com/ungs/formar/persistencia/entidades/Curso.java
+++ src/main/java/com/ungs/formar/persistencia/entidades/Curso.java
@@ -39,6 +39,116 @@ public class Curso {
 		
 	}
 
+	public int getCursoID() {
+		return cursoID;
+	}
 
+	public void setCursoID(int cursoID) {
+		this.cursoID = cursoID;
+	}
+
+	public int getCupoMinimo() {
+		return cupoMinimo;
+	}
+
+	public void setCupoMinimo(int cupoMinimo) {
+		this.cupoMinimo = cupoMinimo;
+	}
+
+	public int getCupoMaximo() {
+		return cupoMaximo;
+	}
+
+	public void setCupoMaximo(int cupoMaximo) {
+		this.cupoMaximo = cupoMaximo;
+	}
+
+	public int getVacantes() {
+		return vacantes;
+	}
+
+	public void setVacantes(int vacantes) {
+		this.vacantes = vacantes;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public String getContenidoEspecifico() {
+		return contenidoEspecifico;
+	}
+
+	public void setContenidoEspecifico(String contenidoEspecifico) {
+		this.contenidoEspecifico = contenidoEspecifico;
+	}
+
+	public int getCantidadTotalClases() {
+		return cantidadTotalClases;
+	}
+
+	public void setCantidadTotalClases(int cantidadTotalClases) {
+		this.cantidadTotalClases = cantidadTotalClases;
+	}
+
+	public int getHorasTotalClases() {
+		return horasTotalClases;
+	}
+
+	public void setHorasTotalClases(int horasTotalClases) {
+		this.horasTotalClases = horasTotalClases;
+	}
+
+	public int getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(int instructor) {
+		this.instructor = instructor;
+	}
+
+	public int getSala() {
+		return sala;
+	}
+
+	public void setSala(int sala) {
+		this.sala = sala;
+	}
+
+	public int getPrograma() {
+		return programa;
+	}
+
+	public void setPrograma(int programa) {
+		this.programa = programa;
+	}
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+
+	public int getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(int responsable) {
+		this.responsable = responsable;
+	}
 	
 }
