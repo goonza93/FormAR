fb09e446a790a8fb8e4964ee65cc38bb61fcab00
Carlos <carloscaballeromorel@gmail.com>
2018-10-07 20:08:08 -0300

    Se agrego traer horario segun ID que recibe un ID y devulve horario

--- src/main/java/com/ungs/formar/Main.java
+++ src/main/java/com/ungs/formar/Main.java
@@ -6,10 +6,9 @@ import com.ungs.formar.vista.ventanas.PantallaPrincipal;
 public class Main {
 
 	public static void main(String[] args) {
-
 		PantallaPrincipal ventanaPantallaPrincipal = new PantallaPrincipal();
 		ControladorPantallaPrincipal controlador = new ControladorPantallaPrincipal(ventanaPantallaPrincipal);
 		controlador.inicializar();
-		
 	}
+	
 }
--- src/main/java/com/ungs/formar/negocios/HorarioCursadaManager.java
+++ src/main/java/com/ungs/formar/negocios/HorarioCursadaManager.java
@@ -1,19 +1,15 @@
 package com.ungs.formar.negocios;
 
-import java.sql.Date;
 import java.util.List;
+
 import com.ungs.formar.persistencia.FactoryODB;
-import com.ungs.formar.persistencia.entidades.Curso;
-import com.ungs.formar.persistencia.entidades.Dia;
-import com.ungs.formar.persistencia.entidades.EstadoCurso;
+import com.ungs.formar.persistencia.entidades.Horario;
 import com.ungs.formar.persistencia.entidades.HorarioCursada;
-import com.ungs.formar.persistencia.interfacesOBD.CursoODB;
-import com.ungs.formar.persistencia.interfacesOBD.DiaOBD;
-import com.ungs.formar.persistencia.interfacesOBD.EstadoCursoOBD;
+import com.ungs.formar.persistencia.interfacesOBD.HorarioOBD;
 
 public class HorarioCursadaManager {
 	
-	public static void crearHorarioCursada(){
+	public static void crearHorarioCursada() {
 			
 	}
 	
@@ -27,4 +23,9 @@ public class HorarioCursadaManager {
 		return null;
 	}
 
+	public static Horario traerHorarioSegunID(Integer ID) {
+		HorarioOBD obd = FactoryODB.crearHorarioOBD();
+		return obd.selectByID(ID);
+	}
+		
 }
