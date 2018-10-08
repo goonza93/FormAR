28a10224411252a1000d05bd845798bbfe8a9b20
Gonzalo Brückmann <goonza.93@live.com.ar>
2018-10-07 21:31:43 -0300

    creada funcion de calcularFechaFin, bd.txt domingo 1er dia sem

--- src/main/java/com/ungs/formar/negocios/CursoManager.java
+++ src/main/java/com/ungs/formar/negocios/CursoManager.java
@@ -1,11 +1,16 @@
 package com.ungs.formar.negocios;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.ungs.formar.persistencia.FactoryODB;
 import com.ungs.formar.persistencia.entidades.Curso;
 import com.ungs.formar.persistencia.entidades.EstadoCurso;
+import com.ungs.formar.persistencia.entidades.Horario;
 import com.ungs.formar.persistencia.entidades.HorarioCursada;
 import com.ungs.formar.persistencia.interfacesOBD.CursoODB;
 import com.ungs.formar.persistencia.interfacesOBD.EstadoCursoOBD;
@@ -57,19 +62,60 @@ public class CursoManager {
 	
 	public static Date calcularFechaFin(List<HorarioCursada> hc, Integer horas, Date fechaInicio){
 		boolean[] dias = {false,false,false,false,false,false,false};
-		Integer[] horasdia = {0,0,0,0,0,0,0};
+		Integer[] minutosdia = {0,0,0,0,0,0,0};
 		//encontrarlos.
+		for(HorarioCursada a : hc){
+			Horario b = HorarioCursadaManager.traerHorarioSegunID(a.getHorario());
+			dias[b.getDia()-1] = true;
+			minutosdia[b.getDia()-1] += (diferencia(b.getHoraInicio(),b.getHoraFin()
+					,b.getMinutoInicio(),b.getMinutoFin())); //falta traducir horarios a matematica
+		}
 		
-		return buscarFechaFin(fechaInicio, horas,dias,horasdia);
+		return buscarFechaFin(fechaInicio, horas,dias,minutosdia);
 		
-	}
-
-	private static Date buscarFechaFin(Date fechaInicio, Integer horas,boolean[] dia,Integer[] horasdia){
-		Date asd = new Date(1);
-		// algoritmo pensado
-		return asd;
 	}
 	
+	private static Integer diferencia(Integer hIn, Integer hFin, Integer mIn, Integer mFin){
+		String dateStart = "11/03/14 "+String.valueOf(hIn)+":"+String.valueOf(mIn)+":00";
+		String dateStop = "11/03/14 "+String.valueOf(hFin)+":"+String.valueOf(mFin)+":00";
+
+		// Custom date format
+		SimpleDateFormat format = new SimpleDateFormat("yy/MM/dd HH:mm:ss");  
+
+		java.util.Date d1 = null;
+		java.util.Date d2 = null;
+		try {
+		    d1 = format.parse(dateStart);
+		    d2 = format.parse(dateStop);
+		} catch (ParseException e) {
+		    e.printStackTrace();
+		}
+		long diff = d2.getTime() - d1.getTime();
+		Integer minutes = (int) TimeUnit.MILLISECONDS.toMinutes(diff);
+		return minutes;
+	}
+	
+	
+
+	private static Date buscarFechaFin(Date fechaInicio, Integer horas,boolean[] dia,Integer[] minutosdia){
+		Integer totalDias = 0;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(fechaInicio);
+		Integer quedan = horas*60; // pase a minutos asi es mas facil
+		while ((quedan)>0){
+			int indexActual = cal.get(Calendar.DAY_OF_WEEK)-1; //indexActual es el dia de la semanaActual
+			if(dia[indexActual]){
+				quedan -= minutosdia[indexActual];
+			}
+			cal.add(Calendar.DATE, 1);
+			totalDias++;
+		}
+		
+		// reseteo el cal denuevo para reusarlo en el calculo final
+		cal.setTime(fechaInicio);
+		cal.add(Calendar.DATE, totalDias);
+		return new Date(cal.getTime().getTime()); // doble get time*** de Calendar -> util.date -> long para tener .sql.date
+	}
 	
 	
 }
