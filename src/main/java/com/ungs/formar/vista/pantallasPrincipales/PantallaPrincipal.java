86f1db3da27c8e78e280ec139b3fda85515187e9
Carlos <carloscaballeromorel@gmail.com>
2018-11-17 01:36:42 -0300

    Fix ventana interna.

--- src/main/java/com/ungs/formar/vista/pantallasPrincipales/PantallaPrincipal.java
+++ src/main/java/com/ungs/formar/vista/pantallasPrincipales/PantallaPrincipal.java
@@ -31,45 +31,32 @@ public class PantallaPrincipal extends JFrame {
 	, menuRecadosConsultarRecibidos, menuRecadosConsultarEnviados, menuRecadosConsultarArchivados
 	, menuUsuariosCrearUsuario, menuUsuariosConsultarUsuarios;
 	private JMenuBar notifBar, barraPrincipal;
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PantallaPrincipal frame = new PantallaPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JMenuBar barrarNuevo;
 
 	public PantallaPrincipal() {
-		// temporal sesion supervisor
+
 		Sesion.setEmpleado(EmpleadoManager.traerEmpleado(8));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Ventana principal");
 		setVisible(true);
-		JFrame asd = new JFrame();
+		//Font f = new Font("sans-serif", Font.PLAIN, 25);
+		//UIManager.put("Menu.font", f);
 		
-		Font f = new Font("sans-serif", Font.PLAIN, 25);
-		UIManager.put("Menu.font", f);
+		//barraPrincipal = crearBarra();
+		barrarNuevo = crearBarra();
+		//notifBar = new JMenuBar();
+		//menuItemNotificacion = new JMenuItem("Notificaciones");
+		//menuItemNotificacion.setFont(new Font("sans-serif", Font.PLAIN, 25));
+		//notifBar.add(menuItemNotificacion);
 		
-		barraPrincipal = crearBarra();
-		
-		notifBar = new JMenuBar();
-		menuItemNotificacion = new JMenuItem("Notificaciones");
-		menuItemNotificacion.setFont(new Font("sans-serif", Font.PLAIN, 25));
-		notifBar.add(menuItemNotificacion);
-		
-		JPanel menuPanel = new JPanel( new BorderLayout() );
-        menuPanel.add(barraPrincipal, BorderLayout.CENTER);
-        menuPanel.add(notifBar, BorderLayout.EAST);
-        add(menuPanel, BorderLayout.NORTH);
+		//JPanel menuPanel = new JPanel( new BorderLayout() );
+        //menuPanel.add(barraPrincipal, BorderLayout.CENTER);
+        //menuPanel.add(notifBar, BorderLayout.EAST);
+        //add(menuPanel, BorderLayout.NORTH);
+		setJMenuBar(barrarNuevo);
 
-		repaint();
-		pack();
+		//repaint();
+		//pack();
 		setLocationRelativeTo(null);
 		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setResizable(false);
@@ -82,42 +69,47 @@ public class PantallaPrincipal extends JFrame {
 			public void mouseClicked(MouseEvent arg0) {}
 		});
 		
-		// creo los items de menu
-		// items menu archivo
+		// MENU ARCHIVO
 		menuArchivoCambiarContrasena = new JMenuItem("Cambiar contraseña");
 		menuArchivoCambiarEmailSistema = new JMenuItem("Cambiar E-Mail del sistema");
 		menuArchivoImportarBD = new JMenuItem("Importar base de datos");
 		menuArchivoExportarBD = new JMenuItem("Exportar base de datos");
 		menuArchivoCerrarSesion = new JMenuItem("Cerrar sesion");
 		menuArchivoSalir = new JMenuItem("Salir");
-		// items menu alumnos
+
+		// MENU ALUMNOS
 		menuAlumnoCrearAlumno = new JMenuItem("Crear alumno");
 		menuAlumnoConsultarAlumnos = new JMenuItem("Consultar alumnos");
 		menuAlumnoConsultarPagos = new JMenuItem("Consultar pagos");
-		// items menu cursos
+
+		// MENU CURSOS
 		menuCursosCrearCurso = new JMenuItem("Crear curso");
 		menuCursosCrearCursada = new JMenuItem("Crear cursada");
 		menuCursosConsultarCursos = new JMenuItem("Consultar cursos");
 		menuCursosConsultarCursadas = new JMenuItem("Consultar cursadas");
 		menuCursosConsultarSalas = new JMenuItem("Consultar salas");
-		//items menu Contactos
+		
+		// MENU CONTACTOS
 		menuContactosCrearContacto = new JMenuItem("Crear contacto");
 		menuContactosConsultarContactos = new JMenuItem("Consultar contactos");
-		// items menu Tareas
+		
+		// MENU TAREAS
 		menuTareasCrearTarea = new JMenuItem("Crear tarea");
 		menuTareasConsultarTareas = new JMenuItem("Consultar tareas");
-		//items menu recados
+		
+		// MENU RECADOS
 		menuRecadosCrearRecado = new JMenuItem("Enviar recado");
 		menuRecadosConsultarRecibidos = new JMenuItem("Consultar recibidos");
 		menuRecadosConsultarEnviados = new JMenuItem("Consultar enviados");
 		menuRecadosConsultarArchivados = new JMenuItem("Consultar archivados");
-		// items menu usuarios
+		
+		// MENU USUARIOS
 		menuUsuariosCrearUsuario = new JMenuItem("Crear usuario");
 		menuUsuariosConsultarUsuarios = new JMenuItem("Consultar usuarios");
 		
 		// los agrego a su menu correspondiente
 		JMenu menuArchivo = new JMenu("Archivo");
-		menuArchivo.setPreferredSize(new Dimension(100,50));
+		//menuArchivo.setPreferredSize(new Dimension(100,50));
 		JMenu menuAlumnos = new JMenu("  Alumnos");
 		JMenu menuCursos = new JMenu("  Cursos");
 		JMenu menuContactos = new JMenu("  Contactos");
@@ -177,223 +169,108 @@ public class PantallaPrincipal extends JFrame {
 		return menuArchivoCambiarContrasena;
 	}
 
-	public void setMenuArchivoCambiarContrasena(
-			JMenuItem menuArchivoCambiarContrasena) {
-		this.menuArchivoCambiarContrasena = menuArchivoCambiarContrasena;
-	}
-
 	public JMenuItem getMenuArchivoCambiarEmailSistema() {
 		return menuArchivoCambiarEmailSistema;
-	}
-
-	public void setMenuArchivoCambiarEmailSistema(
-			JMenuItem menuArchivoCambiarEmailSistema) {
-		this.menuArchivoCambiarEmailSistema = menuArchivoCambiarEmailSistema;
 	}
 
 	public JMenuItem getMenuArchivoImportarBD() {
 		return menuArchivoImportarBD;
 	}
 
-	public void setMenuArchivoImportarBD(JMenuItem menuArchivoImportarBD) {
-		this.menuArchivoImportarBD = menuArchivoImportarBD;
-	}
-	
 	public JMenuItem getMenuArchivoSalir(){
 		return menuArchivoSalir;
-	}
-	
-	public void setMenuArchivoSalir(JMenuItem menuArchivoSalir){
-		this.menuArchivoSalir = menuArchivoSalir;
 	}
 
 	public JMenuItem getMenuAlumnoCrearAlumno() {
 		return menuAlumnoCrearAlumno;
 	}
 
-	public void setMenuAlumnoCrearAlumno(JMenuItem menuAlumnoCrearAlumno) {
-		this.menuAlumnoCrearAlumno = menuAlumnoCrearAlumno;
-	}
-
 	public JMenuItem getMenuAlumnoConsultarAlumnos() {
 		return menuAlumnoConsultarAlumnos;
-	}
-
-	public void setMenuAlumnoConsultarAlumnos(JMenuItem menuAlumnoConsultarAlumnos) {
-		this.menuAlumnoConsultarAlumnos = menuAlumnoConsultarAlumnos;
 	}
 
 	public JMenuItem getMenuItemNotificacion() {
 		return menuItemNotificacion;
 	}
 
-	public void setMenuItemNotificacion(JMenuItem menuItemNotificacion) {
-		this.menuItemNotificacion = menuItemNotificacion;
-	}
-
 	public JMenuItem getMenuArchivoExportarBD() {
 		return menuArchivoExportarBD;
-	}
-
-	public void setMenuArchivoExportarBD(JMenuItem menuArchivoExportarBD) {
-		this.menuArchivoExportarBD = menuArchivoExportarBD;
 	}
 
 	public JMenuItem getMenuArchivoCerrarSesion() {
 		return menuArchivoCerrarSesion;
 	}
 
-	public void setMenuArchivoCerrarSesion(JMenuItem menuArchivoCerrarSesion) {
-		this.menuArchivoCerrarSesion = menuArchivoCerrarSesion;
-	}
-
 	public JMenuItem getMenuAlumnoConsultarPagos() {
 		return menuAlumnoConsultarPagos;
-	}
-
-	public void setMenuAlumnoConsultarPagos(JMenuItem menuAlumnoConsultarPagos) {
-		this.menuAlumnoConsultarPagos = menuAlumnoConsultarPagos;
 	}
 
 	public JMenuItem getMenuCursosCrearCurso() {
 		return menuCursosCrearCurso;
 	}
 
-	public void setMenuCursosCrearCurso(JMenuItem menuCursosCrearCurso) {
-		this.menuCursosCrearCurso = menuCursosCrearCurso;
-	}
-
 	public JMenuItem getMenuCursosCrearCursada() {
 		return menuCursosCrearCursada;
-	}
-
-	public void setMenuCursosCrearCursada(JMenuItem menuCursosCrearCursada) {
-		this.menuCursosCrearCursada = menuCursosCrearCursada;
 	}
 
 	public JMenuItem getMenuCursosConsultarCursos() {
 		return menuCursosConsultarCursos;
 	}
 
-	public void setMenuCursosConsultarCursos(JMenuItem menuCursosConsultarCursos) {
-		this.menuCursosConsultarCursos = menuCursosConsultarCursos;
-	}
-
 	public JMenuItem getMenuCursosConsultarCursadas() {
 		return menuCursosConsultarCursadas;
-	}
-
-	public void setMenuCursosConsultarCursadas(JMenuItem menuCursosConsultarCursadas) {
-		this.menuCursosConsultarCursadas = menuCursosConsultarCursadas;
 	}
 
 	public JMenuItem getMenuCursosConsultarSalas() {
 		return menuCursosConsultarSalas;
 	}
 
-	public void setMenuCursosConsultarSalas(JMenuItem menuCursosConsultarSalas) {
-		this.menuCursosConsultarSalas = menuCursosConsultarSalas;
-	}
-
 	public JMenuItem getMenuContactosCrearContacto() {
 		return menuContactosCrearContacto;
-	}
-
-	public void setMenuContactosCrearContacto(JMenuItem menuContactosCrearContacto) {
-		this.menuContactosCrearContacto = menuContactosCrearContacto;
 	}
 
 	public JMenuItem getMenuContactosConsultarContactos() {
 		return menuContactosConsultarContactos;
 	}
 
-	public void setMenuContactosConsultarContactos(
-			JMenuItem menuContactosConsultarContactos) {
-		this.menuContactosConsultarContactos = menuContactosConsultarContactos;
-	}
-
 	public JMenuItem getMenuTareasCrearTarea() {
 		return menuTareasCrearTarea;
-	}
-
-	public void setMenuTareasCrearTarea(JMenuItem menuTareasCrearTarea) {
-		this.menuTareasCrearTarea = menuTareasCrearTarea;
 	}
 
 	public JMenuItem getMenuTareasConsultarTareas() {
 		return menuTareasConsultarTareas;
 	}
 
-	public void setMenuTareasConsultarTareas(JMenuItem menuTareasConsultarTareas) {
-		this.menuTareasConsultarTareas = menuTareasConsultarTareas;
-	}
-
 	public JMenuItem getMenuRecadosCrearRecado() {
 		return menuRecadosCrearRecado;
-	}
-
-	public void setMenuRecadosCrearRecado(JMenuItem menuRecadosCrearRecado) {
-		this.menuRecadosCrearRecado = menuRecadosCrearRecado;
 	}
 
 	public JMenuItem getMenuRecadosConsultarRecibidos() {
 		return menuRecadosConsultarRecibidos;
 	}
 
-	public void setMenuRecadosConsultarRecibidos(
-			JMenuItem menuRecadosConsultarRecibidos) {
-		this.menuRecadosConsultarRecibidos = menuRecadosConsultarRecibidos;
-	}
-
 	public JMenuItem getMenuRecadosConsultarEnviados() {
 		return menuRecadosConsultarEnviados;
-	}
-
-	public void setMenuRecadosConsultarEnviados(
-			JMenuItem menuRecadosConsultarEnviados) {
-		this.menuRecadosConsultarEnviados = menuRecadosConsultarEnviados;
 	}
 
 	public JMenuItem getMenuRecadosConsultarArchivados() {
 		return menuRecadosConsultarArchivados;
 	}
 
-	public void setMenuRecadosConsultarArchivados(
-			JMenuItem menuRecadosConsultarArchivados) {
-		this.menuRecadosConsultarArchivados = menuRecadosConsultarArchivados;
-	}
-
 	public JMenuItem getMenuUsuariosCrearUsuario() {
 		return menuUsuariosCrearUsuario;
-	}
-
-	public void setMenuUsuariosCrearUsuario(JMenuItem menuUsuariosCrearUsuario) {
-		this.menuUsuariosCrearUsuario = menuUsuariosCrearUsuario;
 	}
 
 	public JMenuItem getMenuUsuariosConsultarUsuarios() {
 		return menuUsuariosConsultarUsuarios;
 	}
 
-	public void setMenuUsuariosConsultarUsuarios(
-			JMenuItem menuUsuariosConsultarUsuarios) {
-		this.menuUsuariosConsultarUsuarios = menuUsuariosConsultarUsuarios;
-	}
-
 	public JMenuBar getNotifBar() {
 		return notifBar;
 	}
 
-	public void setNotifBar(JMenuBar notifBar) {
-		this.notifBar = notifBar;
-	}
-
 	public JMenuBar getBarraPrincipal() {
 		return barraPrincipal;
-	}
-
-	public void setBarraPrincipal(JMenuBar barraPrincipal) {
-		this.barraPrincipal = barraPrincipal;
 	}
 	
 }
